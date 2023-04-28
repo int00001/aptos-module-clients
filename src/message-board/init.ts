b5aee2ef9264e633658e8782d949a4140548549c
int <int_eth@proton.me>
2023-04-27 23:35:14 -0500

    feat(message-board): add msg board init method

---
+++ src/message-board/init.ts
@@ -0,0 +1,44 @@
+import { AptosClient, TxnBuilderTypes } from 'aptos';
+
+import { TESTNET_NODE_URL } from 'config';
+import { getSequence, loadAccount } from 'utils';
+
+const {
+  ChainId,
+  EntryFunction,
+  RawTransaction,
+  AccountAddress,
+  TransactionPayloadEntryFunction,
+} = TxnBuilderTypes;
+
+const main = async () => {
+  const client = new AptosClient(TESTNET_NODE_URL);
+  const account = loadAccount();
+
+  const entryFunctionPayload = new TransactionPayloadEntryFunction(
+    EntryFunction.natural(
+      `${account.address()}::acl_mb`,
+      'message_board_init',
+      [],
+      []
+    )
+  );
+
+  const { sequenceNumber, chainId } = await getSequence(client, account);
+
+  const rawTx = new RawTransaction(
+    AccountAddress.fromHex(account.address()),
+    BigInt(sequenceNumber),
+    entryFunctionPayload,
+    BigInt(2000),
+    BigInt(100),
+    BigInt(Math.floor(Date.now() / 1000) + 10),
+    new ChainId(chainId)
+  );
+
+  const bcsTx = AptosClient.generateBCSTransaction(account, rawTx);
+  const res = await client.submitSignedBCSTransaction(bcsTx);
+  await client.waitForTransaction(res.hash);
+};
+
+main();
